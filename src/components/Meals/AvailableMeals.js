79dcdb71fa941525eb440b6a6e938d53b49df54c
Adarsh Chakraborty <adarshchak108@gmail.com>
2021-11-07 16:15:25 +0530

    Moved data to backend, Added Fetch API

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,52 +1,66 @@
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
-
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import { useEffect, useState } from 'react';
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      id={meal.id}
-      key={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+	const [meals, setMeals] = useState([]);
 
-  return (
-    <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
-    </section>
-  );
+	const [isLoading, setIsLoading] = useState(true);
+	// const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		const fetchMeals = async () => {
+			const res = await fetch(
+				'https://fir-demo-a3f3e-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json'
+			);
+			const mealsObj = await res.json();
+
+			const loadedMeals = [];
+
+			for (const key in mealsObj) {
+				console.log(key);
+				loadedMeals.push({
+					id: key,
+					name: mealsObj[key].name,
+					description: mealsObj[key].description,
+					price: mealsObj[key].price
+				});
+			}
+
+			setMeals(loadedMeals);
+			setIsLoading(false);
+		};
+
+		fetchMeals();
+	}, []);
+
+	// useEffect never runs again cuz no dependencies.
+	// No props, No external dependencies.
+	if (isLoading) {
+		return (
+			<section className={classes.mealsLoading}>
+				<p>Loading...</p>
+			</section>
+		);
+	}
+	const mealsList = meals.map((meal) => (
+		<MealItem
+			id={meal.id}
+			key={meal.id}
+			name={meal.name}
+			description={meal.description}
+			price={meal.price}
+		/>
+	));
+
+	return (
+		<section className={classes.meals}>
+			<Card>
+				<ul>{mealsList}</ul>
+			</Card>
+		</section>
+	);
 };
 
 export default AvailableMeals;
